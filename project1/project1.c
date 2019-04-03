6504ae3f21d857926184a1d8ee2861f0b3290197
Vecto <Vecto@10.222.165.239>
2019-04-03 17:08:22 +1100

    rotation cipher encrypt and decrypt working

--- project1/project1.c
+++ project1/project1.c
@@ -7,8 +7,40 @@
 
 #include <stdio.h>
 
+void *rotEncrypt(char message[], int key);
+void *rotDecrypt(char message[], int key);
+
 int main() {
+	char message[] = "USZ BOE EFDPEF NF CCZ";
+	int key = 1;
+	rotEncrypt(message, key);
+	rotDecrypt(message, key);
+}
 
+void *rotEncrypt(char message[], int key) {
+	char encrypted[1024];
+	for(int i = 0; message[i] != '\0'; ++i) {
+		char letter = message[i];
+		if (letter >= 'a' && letter <= 'z')
+			letter = letter -32;
+		if (letter >= 'A' && letter <= 'Z')
+			letter = letter + key;
+		encrypted[i] = letter;
+		}
+	printf("%s", encrypted);
+}
 
+void *rotDecrypt(char message[], int key) {
+	key = key*-1;
+	char decrypted[1024];
+		for(int i = 0; message[i] != '\0'; ++i) {
+			char letter = message[i];
+			if (letter >= 'a' && letter <= 'z')
+				letter = letter -32;
+			if (letter >= 'A' && letter <= 'Z')
+				letter = letter + key;
+			decrypted[i] = letter;
+			}
+		printf("%s", decrypted);
 }
 
