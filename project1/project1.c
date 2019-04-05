388c8af21bb0c9d2e726e2aef40cc9ea90d4a5cb
Vecto <Vecto@LAPTOP-GF4VS4VF.lan>
2019-04-05 17:55:49 +1100

    rotation and substitution encryption and decryption with key

--- project1/project1.c
+++ project1/project1.c
@@ -6,41 +6,98 @@
  */
 
 #include <stdio.h>
+#include <string.h>
 
-void *rotEncrypt(char message[], int key);
-void *rotDecrypt(char message[], int key);
+void rotEncrypt(char message[], int rotKey);
+void rotDecrypt(char message[], int rotKey);
+void subEncrypt(char message[], char subKey[], char alphabet[]);
+void subDecrypt(char message[], char subKey[], char alphabet[]);
 
 int main() {
-	char message[] = "USZ BOE EFDPEF NF CCZ";
-	int key = 1;
-	rotEncrypt(message, key);
-	rotDecrypt(message, key);
+	char message[] = "hello world";
+	int length = strlen(message);
+	int rotKey = 0;
+	char alphabet[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char subKey[]   = "QWERTYUIOPASDFGHJKLZXCVBNM";
+	printf("Original message: %s\n", message);
+
+	// convert input to upper case
+	for(int i = 0; message[i] != '\0'; i++) {
+		if (message[i] >= 'a' && message[i] <= 'z')
+			message[i] += -32;
+	}
+
+
+	//rotEncrypt(message, rotKey);
+	//rotDecrypt(message, rotKey);
+	//subEncrypt(message, subKey, alphabet);
+	subDecrypt(message, subKey, alphabet);
 }
 
-void *rotEncrypt(char message[], int key) {
-	char encrypted[1024];
-	for(int i = 0; message[i] != '\0'; ++i) {
-		char letter = message[i];
-		if (letter >= 'a' && letter <= 'z')
-			letter = letter -32;
-		if (letter >= 'A' && letter <= 'Z')
-			letter = letter + key;
-		encrypted[i] = letter;
+/***********************************************************************************************/
+//Rotation cipher encryption
+
+void rotEncrypt(char message[], int rotKey) {
+	for(int i = 0; message[i] != '\0'; i++) {
+		if (message[i] >= 'A' && message[i] <= 'Z') {
+			message[i] = (message[i]-65 + rotKey)%26 + 65;
 		}
-	printf("%s", encrypted);
+	}
+	printf("Encrypted message: %s\n", message);
 }
 
-void *rotDecrypt(char message[], int key) {
-	key = key*-1;
-	char decrypted[1024];
+
+
+/***********************************************************************************************/
+// Rotation cipher decryption
+
+void rotDecrypt(char message[], int rotKey) {
 		for(int i = 0; message[i] != '\0'; ++i) {
-			char letter = message[i];
-			if (letter >= 'a' && letter <= 'z')
-				letter = letter -32;
-			if (letter >= 'A' && letter <= 'Z')
-				letter = letter + key;
-			decrypted[i] = letter;
+			if (message[i] >= 'A' && message[i] <= 'Z') {
+				message[i] = (message[i]-65 + (26-rotKey))%26 + 65;
 			}
-		printf("%s", decrypted);
+		}
+		printf("Decrypted message: %s\n", message);
+}
+
+/***********************************************************************************************/
+// Substitution cipher encryption
+void subEncrypt(char message[], char subKey[], char alphabet[]) {
+	int i, j;
+	for(i = 0, j = 0; message[i] != '\0';) {
+		if (message[i] >= 'A' && message[i] <= 'Z') {
+			if (message[i] == alphabet[j]) {
+				message[i] = subKey[j];
+				i++;
+				j=0;
+			}
+			else
+				j++;
+		}
+		else
+		i++;
+		//printf("%d\n", message[i]);
+		}
+	printf("Encrypted message: %s", message);
+}
+
+/***********************************************************************************************/
+// Substitution cipher decryption
+void subDecrypt(char message[], char subKey[], char alphabet[]) {
+	for(int i = 0, j = 0; message[i] != '\0';) {
+		if (message[i] >= 'A' && message[i] <= 'Z') {
+			if (message[i] == subKey[j]) {
+				message[i] = alphabet[j];
+				i++;
+				j = 0;
+			}
+			else
+				j++;
+		}
+		else
+		i++;
+		//printf("%d\n", message[i]);
+		}
+	printf("Decrypted message: %s", message);
 }
 
